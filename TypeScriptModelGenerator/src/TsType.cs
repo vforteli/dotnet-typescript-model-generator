237204edcdf8cd8fd76e0cd191b8c441d92a0493
Verner Fortelius <verner@fortelius.net>
2024-11-11 22:21:07 +0200

    dictionary types to record

--- TypeScriptModelGenerator/src/TsType.cs
+++ TypeScriptModelGenerator/src/TsType.cs
@@ -8,3 +8,10 @@ public abstract record TsType(string Name, bool IsNullable, bool IsArray)
 public record PrimitiveType(string Name, bool IsNullable, bool IsArray) : TsType(Name, IsNullable, IsArray);
 
 public record ComplexType(string Name, bool IsNullable, bool IsArray) : TsType(Name, IsNullable, IsArray);
+
+public record GenericPrimitiveType(
+    string Name,
+    bool IsNullable,
+    bool IsArray,
+    List<TsType> GenericTypes
+) : PrimitiveType(Name, IsNullable, IsArray);
